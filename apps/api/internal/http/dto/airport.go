caf39706e83211f9e954be360f879585a4c88b34
Asif Abbasov <aassifabbasov@gmail.com>
2026-06-23 20:28:11 +0400

    backend: add airport response dto

---
+++ apps/api/internal/http/dto/airport.go
@@ -0,0 +1,11 @@
+package dto
+
+type AirportListItem struct {
+	ICAOCode  string  `json:"icao_code"`
+	IATACode  string  `json:"iata_code"`
+	Name      string  `json:"name"`
+	City      string  `json:"city"`
+	Country   string  `json:"country"`
+	Latitude  float64 `json:"latitude"`
+	Longitude float64 `json:"longitude"`
+}
--- apps/api/internal/http/handlers/airports.go
+++ apps/api/internal/http/handlers/airports.go
@@ -1,20 +1,21 @@
 package handlers
 
 import (
+	"github.com/AsifAbbasov/global-flight-analytics/apps/api/internal/http/dto"
 	"github.com/AsifAbbasov/global-flight-analytics/apps/api/internal/http/response"
 	"github.com/gofiber/fiber/v2"
 )
 
 func ListAirports(c *fiber.Ctx) error {
-	return response.OK(c, []fiber.Map{
+	return response.OK(c, []dto.AirportListItem{
 		{
-			"icao_code": "UBBB",
-			"iata_code": "GYD",
-			"name":      "Heydar Aliyev International Airport",
-			"city":      "Baku",
-			"country":   "Azerbaijan",
-			"latitude":  40.4675,
-			"longitude": 50.0467,
+			ICAOCode:  "UBBB",
+			IATACode:  "GYD",
+			Name:      "Heydar Aliyev International Airport",
+			City:      "Baku",
+			Country:   "Azerbaijan",
+			Latitude:  40.4675,
+			Longitude: 50.0467,
 		},
 	})
 }
